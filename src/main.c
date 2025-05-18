d10ae9272f54c95cb87471ac530b434b887d2b71
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-05-18 20:36:46 +0200

    ADD: Tokenization + Version commad

--- src/main.c
+++ src/main.c
@@ -5,29 +5,48 @@
 
 #define MAX_INPUT_SIZE 1024
 
+#define BLACK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define PURPLE "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define WHITE "\033[0;37m"
+#define RESET "\033[0m"
+
 int main() {
-  printf("Pipwn-shell: 0.01\n");  
-  char command[MAX_INPUT_SIZE];
-  const char *delimiter = " ";
+  while (1) {
+    printf(CYAN "Pipwn-shell> " RESET);
+    fflush(stdout);
 
-  char *token;
+    char command[MAX_INPUT_SIZE];
+    char *version = "version?";
+    char *help = "help?";
 
-  fgets(command, sizeof(command), stdin);
-  printf("------------------------------\n");
-  printf("%s", command);
-  
-  token = strtok(command, delimiter);
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0'; // Remove newline
 
-  while (token) {
-    printf("%s\n", token);
-    token = strtok(NULL, delimiter);
+    if (strcmp(command, version) == 0) {
+      char path[256];
+      snprintf(path, sizeof(path), "%s/Desktop/pipwn-shell/src/version.txt", getenv("HOME"));
+      FILE *Vptr = fopen("version.txt", "r");
+      if (!Vptr) {
+        perror(RED "Couldn't open version.txt" RESET);
+        continue;
+      }
+
+      char ch;
+      printf(GREEN); // Set color
+      while ((ch = fgetc(Vptr)) != EOF) {
+        putchar(ch);
+      }
+      printf(RESET); // Reset color
+
+      fclose(Vptr);
+    }
   }
-
-  /*
-  int cmd_len = strlen(command);
-  for (int i = 0; i < cmd_len; i++) {
-    printf("%c\n", command[i]);
-  }*/
 
   return 0;
 }
+
