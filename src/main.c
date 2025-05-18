72833d82e651a8dca8b6728971c3812b2af2d506
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-05-18 08:27:36 +0200

    ADD: Tokenization of User INPUT

--- src/main.c
+++ src/main.c
@@ -8,16 +8,26 @@
 int main() {
   printf("Pipwn-shell: 0.01\n");  
   char command[MAX_INPUT_SIZE];
+  const char *delimiter = " ";
+
+  char *token;
 
   fgets(command, sizeof(command), stdin);
   printf("------------------------------\n");
   printf("%s", command);
+  
+  token = strtok(command, delimiter);
 
-  int spaces;
+  while (token) {
+    printf("%s\n", token);
+    token = strtok(NULL, delimiter);
+  }
+
+  /*
   int cmd_len = strlen(command);
   for (int i = 0; i < cmd_len; i++) {
     printf("%c\n", command[i]);
-  }
+  }*/
 
   return 0;
 }
