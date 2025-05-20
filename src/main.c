fe90f3a2e1511e3cd0515cd4694aff410cb7d9b8
Gustavo <gustavo@localhost.localdomain>
2025-05-20 15:35:21 +0200

    FIX: fixed open file bug

--- src/main.c
+++ src/main.c
@@ -2,51 +2,52 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
+#include <limits.h>
 
 #define MAX_INPUT_SIZE 1024
 
-#define BLACK "\033[0;30m"
-#define RED "\033[0;31m"
-#define GREEN "\033[0;32m"
-#define YELLOW "\033[0;33m"
-#define BLUE "\033[0;34m"
-#define PURPLE "\033[0;35m"
 #define CYAN "\033[0;36m"
-#define WHITE "\033[0;37m"
+#define GREEN "\033[0;32m"
+#define RED "\033[0;31m"
 #define RESET "\033[0m"
 
-int main() {
-  while (1) {
-    printf(CYAN "Pipwn-shell> " RESET);
-    fflush(stdout);
+int main(int argc, char *argv[]) {
+    while (1) {
+        printf(CYAN "Pipwn-shell> " RESET);
+        fflush(stdout);
 
-    char command[MAX_INPUT_SIZE];
-    char *version = "version?";
-    char *help = "help?";
+        char command[MAX_INPUT_SIZE];
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = '\0'; // Remove newline
 
-    fgets(command, sizeof(command), stdin);
-    command[strcspn(command, "\n")] = '\0'; // Remove newline
+        if (strcmp(command, "version?") == 0) {
+            char exe_path[PATH_MAX];
+            if (realpath(argv[0], exe_path) == NULL) {
+                perror(RED "Error getting executable path" RESET);
+                continue;
+            }
 
-    if (strcmp(command, version) == 0) {
-      char path[256];
-      snprintf(path, sizeof(path), "%s/Desktop/pipwn-shell/src/version.txt", getenv("HOME"));
-      FILE *Vptr = fopen("version.txt", "r");
-      if (!Vptr) {
-        perror(RED "Couldn't open version.txt" RESET);
-        continue;
-      }
+            char *exe_dir = dirname(exe_path);
+            char version_path[PATH_MAX];
+            snprintf(version_path, sizeof(version_path), "%s/../src/version.txt", exe_dir);
 
-      char ch;
-      printf(GREEN); // Set color
-      while ((ch = fgetc(Vptr)) != EOF) {
-        putchar(ch);
-      }
-      printf(RESET); // Reset color
+            FILE *Vptr = fopen(version_path, "r");
+            if (!Vptr) {
+                perror(RED "Couldn't open version.txt" RESET);
+                continue;
+            }
 
-      fclose(Vptr);
+            char ch;
+            printf(GREEN);
+            while ((ch = fgetc(Vptr)) != EOF) {
+                putchar(ch);
+            }
+            printf(RESET);
+            fclose(Vptr);
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
 
